cd55b72ca07eb9af2f35e7caf31673d588342b31
Shanell Brown <shanellbrown332@gmail.com>
2018-02-19 03:06:39 +0000

    sent the mail

--- app/__init__.py
+++ app/__init__.py
@@ -6,8 +6,8 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'passphrase'
 app.config['MAIL_SERVER'] = 'smtp.mailtrap.io'
 app.config['MAIL_PORT'] = '2525'
-app.config['MAIL_USERNAME'] ='9e8174f5085ea0'
-app.config['MAIL_PASSWORD'] = '7fb25a3e5ebba3'
+app.config['MAIL_USERNAME'] =''
+app.config['MAIL_PASSWORD'] = ''
 
 mail = Mail(app)
 from app import views
--- app/forms.py
+++ app/forms.py
@@ -3,7 +3,7 @@ from wtforms import StringField
 from wtforms.validators import DataRequired
 
 class MyForm(FlaskForm):
-    name = StringField('name', validators=[DataRequired()])
-    email = StringField('email', validators=[DataRequired()])
-    subject = StringField('subject', validators=[DataRequired()])
-    textArea = StringField('textArea', validators=[DataRequired()])
+    name = StringField('Name', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired()])
+    subject = StringField('Subject', validators=[DataRequired()])
+    textArea = StringField('Message', validators=[DataRequired()])
